7712bba8b60ab64addb2a13d236183f1107a3c1a
Nelson_BOTERO <nelsonbotero@gmail.com>
2024-11-24 17:34:10 +0000

    Deleted

--- models/OQ_FINANCE/FINANCE_GOLD/GLD_Material_Modelled_Proccess_Step.sql
+++ models/OQ_FINANCE/FINANCE_GOLD/GLD_Material_Modelled_Proccess_Step.sql
@@ -1,8 +1,8 @@
 select 
-source_country
-, COMPONENT
-,PRODUCT_CATEGORY
+
+COMPONENT
+, PRODUCT_CATEGORY
 , count(*) as NB_Products
 from {{ ref("SLV_Material_Modelled_Proccess_Step") }}
-group by source_country, COMPONENT,PRODUCT_CATEGORY
+group by  COMPONENT,PRODUCT_CATEGORY
 
